3ea6009b187f716d3d3d06de727b0262c29752f3
James Innes <34094735+james-innes@users.noreply.github.com>
2021-12-29 01:06:48 +0000

    Remove global form error state in main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
 import App from "./App.svelte";
 
-document.addEventListener("touchstart", () => {}, false);
-
-document.querySelectorAll("input").forEach(el => {
-	el.addEventListener("invalid", el.classList.add("error"), false);
-	el.addEventListener("blur", el.checkValidity());
-});
-
 const app = new App({
 	target: document.body,
 });
